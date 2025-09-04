c9266071773e5f6d05c16c34295ad9570248b32c
ranahamdan1997 <ranahamdan2297@gmail.com>
2025-09-03 21:47:42 -0500

    test

---
+++ test3sep2025/src/test/java/test3sep2025/test3sep2025.java
@@ -0,0 +1,14 @@
+package test3sep2025;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test3sep2025 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriver driver =new ChromeDriver();
+driver.get("https://www.youtube.com/watch?v=IVqNY-DCzAo");
+	}
+
+}
