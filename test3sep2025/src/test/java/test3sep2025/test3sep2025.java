3b7023996c75854ca96cad4192fd8289904d0b0c
ranahamdan1997 <ranahamdan2297@gmail.com>
2025-09-05 13:49:05 -0500

    Fixed Selenium test to print page title and close driver

--- test3sep2025/src/test/java/test3sep2025/test3sep2025.java
+++ test3sep2025/src/test/java/test3sep2025/test3sep2025.java
@@ -2,13 +2,19 @@ package test3sep2025;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class test3sep2025 {
 
-	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
+		
+		
+		@Test
+		void test1() {
 WebDriver driver =new ChromeDriver();
 driver.get("https://www.youtube.com/watch?v=IVqNY-DCzAo");
-	}
+	
 
+}
 }
