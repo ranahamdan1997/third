5a0910d33d03845cbf98e226584b7b804b073ee4
ranahamdan1997 <ranahamdan2297@gmail.com>
2025-09-05 16:02:02 -0500

    Update test3sep2025.java

--- test3sep2025/src/test/java/test3sep2025/test3sep2025.java
+++ test3sep2025/src/test/java/test3sep2025/test3sep2025.java
@@ -5,14 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class test3sep2025 {
+public class Test3sep2025 {
 
     @Test
-    void test1() {
-        System.out.println("Test started..."); // <-- للتأكد أن الطباعة تظهر
-
+    public void test1() {
         System.setProperty("webdriver.chrome.driver",
-                "/Users/ranahamdan/.cache/selenium/chromedriver/mac-arm64/139.0.7258.154/chromedriver");
+            "/Users/ranahamdan/.cache/selenium/chromedriver/mac-arm64/139.0.7258.154/chromedriver");
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
@@ -23,8 +21,6 @@ public class test3sep2025 {
 
         driver.get("https://www.youtube.com/watch?v=IVqNY-DCzAo");
         System.out.println("THE TITLE IS: " + driver.getTitle());
-
         driver.quit();
-        System.out.println("Test finished...");
     }
 }
