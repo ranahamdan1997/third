48c82337202cb94f8cd0010cc3eb26c9944d4f33
ranahamdan1997 <ranahamdan2297@gmail.com>
2025-09-05 15:59:35 -0500

    Update test3sep2025.java

--- test3sep2025/src/test/java/test3sep2025/test3sep2025.java
+++ test3sep2025/src/test/java/test3sep2025/test3sep2025.java
@@ -7,31 +7,24 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class test3sep2025 {
 
+    @Test
+    void test1() {
+        System.out.println("Test started..."); // <-- للتأكد أن الطباعة تظهر
 
-		// TODO Auto-generated method stub
-		
-		
-		@Test
-		void test1() {
- // تحديد مسار ChromeDriver
         System.setProperty("webdriver.chrome.driver",
                 "/Users/ranahamdan/.cache/selenium/chromedriver/mac-arm64/139.0.7258.154/chromedriver");
 
-        // تفعيل الـ headless mode
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // بدون GUI
+        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
 
         WebDriver driver = new ChromeDriver(options);
 
-        // فتح صفحة
         driver.get("https://www.youtube.com/watch?v=IVqNY-DCzAo");
-
-        // طباعة عنوان الصفحة
         System.out.println("THE TITLE IS: " + driver.getTitle());
 
         driver.quit();
-
-}
+        System.out.println("Test finished...");
+    }
 }
