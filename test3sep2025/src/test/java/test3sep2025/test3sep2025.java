d0393bb6042a544835a1239f617aac24b71f7ded
ranahamdan1997 <ranahamdan2297@gmail.com>
2025-09-08 12:38:49 -0500

    Update test3sep2025.java

--- test3sep2025/src/test/java/test3sep2025/test3sep2025.java
+++ test3sep2025/src/test/java/test3sep2025/test3sep2025.java
@@ -3,18 +3,19 @@ package test3sep2025;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class test3sep2025 {
 
+    @Test
+    public void test1() {
 
-		// TODO Auto-generated method stub
-		
-		
-		@Test
-		void test1() {
-WebDriver driver =new ChromeDriver();
-driver.get("https://www.youtube.com/watch?v=IVqNY-DCzAo");
-	
+        WebDriver driver = new ChromeDriver();
 
-}
+        driver.get("https://www.youtube.com/watch?v=IVqNY-DCzAo");
+        System.out.println("THE TITLE IS: ya rab" + driver.getTitle());
+                System.out.println("THE TITLE IS: ya rab222222222" + driver.getTitle());
+
+        driver.quit();
+    }
 }
